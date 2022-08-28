05127bdcb8ab96eab913d3968e116d5b30625432
jorektheglitch <lozcontrariuz@yandex.ru>
2022-08-28 06:11:14 +0300

    Add X25519-related classes to encryption.py

--- qorp/encryption.py
+++ qorp/encryption.py
@@ -1,2 +1,4 @@
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PublicKey
