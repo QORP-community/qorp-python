44bdc5ccb5b11d92dbf93a9059f6fe844ff1e490
jorektheglitch <lozcontrariuz@yandex.ru>
2022-09-07 22:54:33 +0300

    Simplify Router - leave only messages relaying logic, remove messages processing logic.

--- qorp/router.py
+++ qorp/router.py
@@ -1,16 +1,13 @@
 import asyncio
 from asyncio import Future
-from contextlib import contextmanager
 from dataclasses import dataclass
 from weakref import WeakKeyDictionary
 
-from typing import Callable, Dict, Generator, Optional, Set, Tuple
+from typing import Callable, Dict, Optional, Set, Tuple
 
-from .encryption import Ed25519PrivateKey, Ed25519PublicKey
-from .encryption import X25519PrivateKey
 from .encryption import ChaCha20Poly1305
 from .frontend import Frontend
-from .messages import FrontendData, NetworkMessage
+from .messages import NetworkMessage
 from .messages import NetworkData, RouteRequest, RouteResponse, RouteError
 from .nodes import KnownNode, Node, Neighbour
 from .transports import Listener
@@ -20,7 +17,6 @@ RRepInfo = Tuple[Neighbour, RouteResponse]
 
 RREQ_TIMEOUT = 10
 EMPTY_SET: Set["Future[RRepInfo]"] = set()
-EMPTY_DICT: Dict = {}
 
 
 @dataclass
@@ -35,108 +31,52 @@ class RouteInfo:
         return self.counter
 
 
-class Router(KnownNode):
+class Router:
 
-    private_key: Ed25519PrivateKey
-    public_key: Ed25519PublicKey
-    broadcast_listeners: Set[Listener]
     frontend: Frontend
+    broadcast_listeners: Set[Listener]
     neighbours: Set[Neighbour]
-    incoming_routes: Dict[KnownNode, RouteInfo]
-    outgoing_routes: Dict[KnownNode, RouteInfo]
-    transit_routes: Dict[KnownNode, Dict[KnownNode, Neighbour]]
-    directions: Dict[Node, Neighbour]
+    routes: Dict[Tuple[KnownNode, KnownNode], Tuple[Neighbour, Neighbour]]
+    directions: Dict[KnownNode, Neighbour]
     pending_requests: Dict[Node, Set["Future[RRepInfo]"]]
     _requests_details: WeakKeyDictionary["Future[RRepInfo]", RouteRequest]
 
-    def __init__(self, private_key: bytes, frontend: Frontend) -> None:
-        super().__init__()
-        self.private_key = Ed25519PrivateKey.from_private_bytes(private_key)
-        self.public_key = self.private_key.public_key()
-        # TODO: add router's adsress initialization
-        # self.address =
+    def __init__(self, frontend: Frontend) -> None:
         self.frontend = frontend
         self.broadcast_listeners = set()
-        self.neighbours = set()
-        self.incoming_routes = {}
-        self.outgoing_routes = {}
-        self.transit_routes = {}
+        self.neighbours = {frontend}
+        self.directions = {frontend: frontend}
         self.pending_requests = {}
         self._requests_details = WeakKeyDictionary()
 
-    async def find_direction(self, target: Node, timeout: Optional[float] = RREQ_TIMEOUT) -> Neighbour:
-        if target in self.directions:
-            return self.directions[target]
-        exchange_private_key = X25519PrivateKey.generate()
-        exchange_public_key = exchange_private_key.public_key()
-        request = RouteRequest(self, target, exchange_public_key)
-        with self._track_request(target) as future:
-            for neighbour in self.neighbours:
-                neighbour.send(request)
-            direction, response = await asyncio.wait_for(future, timeout)
-            destination_public_key = response.public_key
-            raw_encryption_key = exchange_private_key.exchange(destination_public_key)
-            encryption_key = ChaCha20Poly1305(raw_encryption_key)
-            route_info = RouteInfo(direction, encryption_key)
-            # TODO: check that there is no existed route info for target
-            self.outgoing_routes[response.source] = route_info
-        return direction
-
-    @contextmanager
-    def _track_request(self, target: Node) -> Generator["Future[RRepInfo]", None, None]:
-        future: Future[RRepInfo] = Future()
-        requests = self.pending_requests.setdefault(target, set())
-        requests.add(future)
-        try:
-            yield future
-        finally:
-            requests.remove(future)
-
-    def network_message_callback(self, source: Neighbour, message: NetworkMessage) -> None:
-        if not message.verify():
+    def message_callback(self, source: Neighbour, msg: NetworkMessage) -> None:
+        if not msg.verify():
             return
-        if isinstance(message, NetworkData):
-            self.handle_data(source, message)
-        elif isinstance(message, RouteRequest):
-            self.handle_rreq(source, message)
-        elif isinstance(message, RouteResponse):
-            self.handle_rrep(source, message)
-        elif isinstance(message, RouteError):
-            self.handle_rerr(source, message)
+        if isinstance(msg, NetworkData):
+            self.handle_data(source, msg)
+        elif isinstance(msg, RouteRequest):
+            self.handle_rreq(source, msg)
+        elif isinstance(msg, RouteResponse):
+            self.handle_rrep(source, msg)
+        elif isinstance(msg, RouteError):
+            self.handle_rerr(source, msg)
         else:
             raise TypeError
 
     def handle_data(self, source: Neighbour, data: NetworkData) -> None:
-        target = data.destination
-        if target == self:
-            route_info = self.incoming_routes[data.source]
-            message_data = route_info.encryption_key.decrypt(data.nonce, data.payload, None)
-            message = FrontendData(data.source, data.destination, message_data)
-            self.frontend.send(message)
-        elif target in self.directions:
-            direction = self.directions[target]
-            direction.send(data)
-        else:
-            rerr = RouteError(self, source, data.source, data.destination)
+        route_pair = data.source, data.destination
+        directions = self.routes.get(route_pair)
+        if directions is None:
+            rerr = RouteError(self.frontend, source, data.source, data.destination)
             source.send(rerr)
+            return
+        source_direction, destination_direction = directions
+        if source_direction == source:
+            destination_direction.send(data)
 
     def handle_rreq(self, source: Neighbour, request: RouteRequest) -> None:
         target = request.destination
-        if target == self:
-            exchange_private_key = X25519PrivateKey.generate()
-            exchange_public_key = exchange_private_key.public_key()
-            source_public_key = request.public_key
-            raw_encryption_key = exchange_private_key.exchange(source_public_key)
-            # NOTE: there is no need to cut 32-bytes shared secret
-            #       because ChaCha20 uses exactly 32-bytes long key
-            encryption_key = ChaCha20Poly1305(raw_encryption_key)
-            route_info = RouteInfo(source, encryption_key)
-            # TODO: check that there is no existed route info for request
-            #       source (it might allow replay attacks)
-            self.incoming_routes[request.source] = route_info
-            response = RouteResponse(self, request.source, source_public_key, exchange_public_key)
-            source.send(response)
-        elif target in self.directions:
+        if target in self.directions:
             direction = self.directions[target]
             direction.send(request)
         else:
@@ -165,11 +105,13 @@ class Router(KnownNode):
             future.set_result((source, response))
 
     def handle_rerr(self, source: Neighbour, error: RouteError) -> None:
-        destinations = self.transit_routes.get(error.route_source, EMPTY_DICT)
-        direction = destinations.get(error.route_destination)
-        if direction == source:
-            destinations.pop(direction)
-        # TODO: ?? resend RErr message
+        route_pair = error.route_source, error.route_destination
+        directions = self.routes.get(route_pair)
+        if not directions or directions[1] != source:
+            return
+        self.routes.pop(route_pair)
+        source_direction = directions[0]
+        source_direction.send(error)
 
     def is_unique_rreq(self, rreq: RouteRequest, exclude: Optional["Future[RRepInfo]"] = None) -> bool:
         target = rreq.destination
@@ -181,10 +123,6 @@ class Router(KnownNode):
             # there is exactly one request and it is excluded request
             return True
         return False
-
-    def frontend_message_callback(self, message: FrontendData) -> None:
-        # TODO: write frontend-originated data message processing code
-        pass
 
     def _rreq_ttl_killer(self, target: Node, future: "Future[RRepInfo]") -> Callable[[], None]:
         def callback() -> None:
@@ -200,10 +138,14 @@ class Router(KnownNode):
             futures = self.pending_requests.get(target, EMPTY_SET)
             if future in futures:
                 futures.remove(future)
-            if future.done() and not (future.cancelled() or future.exception()):
-                result = future.result()
-                direction, _ = result
-                self.directions.setdefault(target, direction)
-                for future in futures:
-                    future.set_result(result)
+            if future.cancelled() or future.exception():
+                return
+            result = future.result()
+            direction, response = result
+            directions = (direction, direction)
+            self.routes[(response.destination, response.source)] = directions
+            self.routes[(response.source, response.destination)] = directions
+            self.directions.setdefault(response.source, direction)
+            for future in futures:
+                future.set_result(result)
         return callback
