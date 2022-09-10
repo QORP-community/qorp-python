3d77182d9db0c6bdc6c904d1ae1f1a7aec7310bc
jorektheglitch <lozcontrariuz@yandex.ru>
2022-09-10 17:49:41 +0300

    Add function for dumping pulic keys to bytes

--- qorp/encoding.py
+++ qorp/encoding.py
@@ -1,10 +1,20 @@
 from abc import ABC, abstractmethod
 
-from typing import Callable
+from typing import Callable, Union
+
+from .encryption import Ed25519PublicKey, X25519PublicKey
+from cryptography.hazmat.primitives import serialization
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from .messages import NetworkMessage
+
+
+def pubkey_to_bytes(key: Union[Ed25519PublicKey, X25519PublicKey]) -> bytes:
+    return key.public_bytes(
+        encoding=serialization.Encoding.Raw,
+        format=serialization.PublicFormat.Raw,
+    )
 
 
 class Encoder(ABC, Callable[["NetworkMessage"], bytes]):
