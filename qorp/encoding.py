472e5235563c258fad849c4a7aa8caf03affea8f
jorektheglitch <lozcontrariuz@yandex.ru>
2022-09-12 07:13:58 +0300

    Add `default_encoder` and `default_decoder` functions

--- qorp/encoding.py
+++ qorp/encoding.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
-from typing import Union
+from typing import Dict, List, Tuple, Type, Union
+from typing import overload
 from typing_extensions import Literal, Protocol
 
 from .encryption import Ed25519PublicKey, X25519PublicKey, pubkey_to_bytes
@@ -8,10 +9,23 @@ from .messages import NetworkData, RouteError, RouteRequest, RouteResponse
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
+    from .nodes import Node, KnownNode
     from .messages import NetworkMessage
 
 
+CHACHA_NONCE_LENGTH = 12
+PUBKEY_LENGTH = 32
+SIGNATURE_LENGTH = 64
 
+TYPE_TO_LABEL: Dict[Type[NetworkMessage], bytes] = {
+    NetworkData: b"\x01",
+    RouteRequest: b"\x02",
+    RouteResponse: b"\x03",
+    RouteError: b"\x04"
+}
+LABEL_TO_TYPE: Dict[bytes, Type[NetworkMessage]] = {
+    label: type for type, label in TYPE_TO_LABEL.items()
+}
 
 
 class Encoder(Protocol):
@@ -30,3 +44,135 @@ class Decoder(Protocol):
 
     def __call__(self, data: bytes) -> NetworkMessage:
         pass
+
+
+def split(source: bytes, *lengths: int) -> List[bytes]:
+    start = 0
+    chunks = []
+    for end in lengths:
+        chunk = source[start:end]
+        chunks.append(chunk)
+        start += end
+    if start < len(source) - 1:
+        chunk = source[start:]
+        chunks.append(chunk)
+    return chunks
+
+
+def default_encoder(message: NetworkMessage) -> bytes:
+    from .nodes import KnownNode  # import here to avoid circular imports
+    fields: List[bytes]
+    message_type = type(message)
+    if isinstance(message, NetworkData):
+        fields = [
+            pubkey_to_bytes(message.source.public_key),
+            pubkey_to_bytes(message.destination.public_key),
+            TYPE_TO_LABEL[message_type],
+            message.nonce,
+            message.length.to_bytes(2, "big"),
+            message.signature,
+            message.payload,
+        ]
+    elif isinstance(message, RouteRequest):
+        if isinstance(message.destination, KnownNode):
+            dst_field = pubkey_to_bytes(message.destination.public_key)
+        else:
+            dst_field = message.destination.address
+        fields = [
+            pubkey_to_bytes(message.source.public_key),
+            dst_field,
+            TYPE_TO_LABEL[message_type],
+            pubkey_to_bytes(message.public_key),
+            message.signature,
+        ]
+    elif isinstance(message, RouteResponse):
+        fields = [
+            pubkey_to_bytes(message.source.public_key),
+            pubkey_to_bytes(message.destination.public_key),
+            TYPE_TO_LABEL[message_type],
+            pubkey_to_bytes(message.requester_key),
+            pubkey_to_bytes(message.public_key),
+            message.signature,
+        ]
+    elif isinstance(message, RouteError):
+        fields = [
+            pubkey_to_bytes(message.source.public_key),
+            pubkey_to_bytes(message.destination.public_key),
+            TYPE_TO_LABEL[message_type],
+            pubkey_to_bytes(message.route_source.public_key),
+            pubkey_to_bytes(message.route_destination.public_key),
+            message.signature,
+        ]
+    else:
+        raise TypeError(f"Unknown message type: {message_type.__name__}")
+    raw = b"".join(fields)
+    return raw
+
+
+def default_decoder(data: bytes) -> NetworkMessage:
+    from .nodes import KnownNode  # import here to avoid circular imports
+    message: NetworkMessage
+    head_scheme = PUBKEY_LENGTH, PUBKEY_LENGTH, 1
+    source_, destination_, type_label, body = split(data, *head_scheme)
+    message_type = LABEL_TO_TYPE.get(type_label)
+    if message_type is None:
+        raise ValueError(f"Incorrect message type label: {type_label!r}")
+    if message_type is NetworkData:
+        source, destination = _decode_sorce_destination(source_, destination_)
+        data_scheme = CHACHA_NONCE_LENGTH, 2, SIGNATURE_LENGTH
+        nonce, length_, signature, payload = split(body, *data_scheme)
+        length = int.from_bytes(length_, "big")
+        message = NetworkData(source, destination, nonce, length, payload)
+    elif message_type is RouteRequest:
+        # TODO: guess there is a full key of target or just a reduced address
+        rreq_scheme = PUBKEY_LENGTH, SIGNATURE_LENGTH
+        pubkey_, signature = split(body, *rreq_scheme)
+        pubkey = X25519PublicKey.from_public_bytes(pubkey_)
+        message = RouteRequest(source, destination, pubkey)
+    elif message_type is RouteResponse:
+        source, destination = _decode_sorce_destination(source_, destination_)
+        rrep_scheme = PUBKEY_LENGTH, PUBKEY_LENGTH, SIGNATURE_LENGTH
+        requester_pubkey_, pubkey_, signature = split(body, *rrep_scheme)
+        requester_pubkey = X25519PublicKey.from_public_bytes(requester_pubkey_)
+        pubkey = X25519PublicKey.from_public_bytes(pubkey_)
+        message = RouteResponse(source, destination, requester_pubkey, pubkey)
+    elif message_type is RouteError:
+        source, destination = _decode_sorce_destination(source_, destination_)
+        rerr_scheme = PUBKEY_LENGTH, PUBKEY_LENGTH, SIGNATURE_LENGTH
+        route_src_, route_dst_, signature = split(body, *rerr_scheme)
+        route_src_key = Ed25519PublicKey.from_public_bytes(route_src_)
+        route_dst_key = Ed25519PublicKey.from_public_bytes(route_dst_)
+        route_src = KnownNode(route_src_key)
+        route_dst = KnownNode(route_dst_key)
+        message = RouteError(source, destination, route_src, route_dst)
+    else:
+        raise ValueError(f"Unknown message type: {message_type.__name__}")
+    message.set_signature(signature)
+    return message
+
+
+@overload
+def _decode_sorce_destination(
+    src: bytes, dst: bytes
+) -> Tuple[KnownNode, KnownNode]: ...
+@overload
+def _decode_sorce_destination(
+    src: bytes, dst: bytes, unknown_dst: Literal[False] = False
+) -> Tuple[KnownNode, KnownNode]: ...
+@overload
+def _decode_sorce_destination(
+    src: bytes, dst: bytes, unknown_dst: Literal[True] = True
+) -> Tuple[KnownNode, Node]: ...
+def _decode_sorce_destination(
+    src: bytes, dst: bytes, unknown_dst: bool = False
+) -> Union[Tuple[KnownNode, Node], Tuple[KnownNode, KnownNode]]:
+    src_key = Ed25519PublicKey.from_public_bytes(src)
+    source = KnownNode(src_key)
+    if unknown_dst:
+        from .nodes import NodeAddress # import here to avoid circular imports
+        destination = Node(NodeAddress(dst))
+    else:
+        dst_key = Ed25519PublicKey.from_public_bytes(dst)
+        destination = KnownNode(dst_key)
+    return source, destination
+
