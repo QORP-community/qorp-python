6266c9e786fbae7e9f2c4667e34683dce58f3767
jorektheglitch <lozcontrariuz@yandex.ru>
2022-09-17 03:25:44 +0300

    Fix incorrect imports

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,9 +1,9 @@
 import unittest
 
 from .test_messages import TestMessageSignVerify
-from .test_messages import TestMessageDefaultEncodingDecoding
+from .test_messages import TestDefaultCodec
 
 
 tests = unittest.TestSuite()
 tests.addTest(unittest.makeSuite(TestMessageSignVerify))
-tests.addTest(unittest.makeSuite(TestMessageDefaultEncodingDecoding))
+tests.addTest(unittest.makeSuite(TestDefaultCodec))
