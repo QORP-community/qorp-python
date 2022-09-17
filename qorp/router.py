f026aa0d0e7b6d7a92e05ed0aac03f74137e91f1
jorektheglitch <lozcontrariuz@yandex.ru>
2022-09-17 19:36:22 +0300

    Remove misplaced docstring

--- qorp/router.py
+++ qorp/router.py
@@ -21,16 +21,6 @@ class SessionInfo:
 
 
 class Router(Neighbour):
-    """
-    Frontend is intermediator between router and OS or some software.
-
-    Frontend obtains Data messages from OS or router, encode-decode it and then
-    rely it to OS (if message comes from router) or router (if message comes
-    from OS).
-
-    `message_callback` is a callback function which must be called on each
-    message that frontend fetches from OS or other software.
-    """
 
     private_key: Ed25519PrivateKey
     public_key: Ed25519PublicKey
